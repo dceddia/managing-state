368ee0e8c43998fbf9f191b2c946445032acade4
Dave Ceddia <dceddia@gmail.com>
2019-07-08 16:23:15 -0400

    Removed constructor

--- 03-constructorless/src/index.js
+++ 03-constructorless/src/index.js
@@ -3,26 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Counter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0
-    };
-    this.increment = this.increment.bind(this);
-    this.decrement = this.decrement.bind(this);
-  }
+  state = {
+    count: 0
+  };
 
-  increment() {
+  increment = () => {
     this.setState({
       count: this.state.count + 1
     });
-  }
+  };
 
-  decrement() {
+  decrement = () => {
     this.setState({
       count: this.state.count - 1
     });
-  }
+  };
 
   render() {
     return (
