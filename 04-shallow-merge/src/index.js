cdb5ac936bf952956c2bf3c509a9ec7e07f0c8ea
Dave Ceddia <dceddia@gmail.com>
2019-07-08 16:59:11 -0400

    Finished example of shallow/deep merge

--- 04-shallow-merge/src/index.js
+++ 04-shallow-merge/src/index.js
@@ -4,32 +4,53 @@ import './index.css';
 
 class Counter extends React.Component {
   state = {
-    count: 0
+    status: {
+      count: 0,
+      lit: true
+    }
   };
 
   increment = () => {
     this.setState({
-      count: this.state.count + 1
+      status: {
+        ...this.state.status,
+        count: this.state.status.count + 1
+      }
     });
   };
 
   decrement = () => {
     this.setState({
-      count: this.state.count - 1
+      status: {
+        ...this.state.status,
+        count: this.state.status.count - 1
+      }
     });
   };
 
+  toggle = () => {
+    this.setState(state => ({
+      status: {
+        ...state.status,
+        lit: !state.status.lit
+      }
+    }));
+  };
+
   render() {
+    const { lit, count } = this.state.status;
+
     return (
-      <div className="counter">
+      <div className={`counter ${lit ? '' : 'dark'}`}>
         <h2>Counter</h2>
         <div>
           <button onClick={this.decrement}>-</button>
-          <span className="count">
-            {this.state.count}
-          </span>
+          <span className="count">{count}</span>
           <button onClick={this.increment}>+</button>
         </div>
+        <button onClick={this.toggle}>
+          Light/Dark
+        </button>
       </div>
     );
   }
