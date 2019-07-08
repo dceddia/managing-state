d4cd203c0a2d4ea132622241ebd4c1d7c66ac751
Dave Ceddia <dceddia@gmail.com>
2019-07-08 11:19:29 -0400

    Stateful counter

--- 01-counter/src/index.js
+++ 01-counter/src/index.js
@@ -2,16 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const Counter = () => (
-  <div className="counter">
-    <h2>Counter</h2>
-    <div>
-      <button>-</button>
-      <span className="count">0</span>
-      <button>+</button>
-    </div>
-  </div>
-);
+class Counter extends React.Component {
+  state = {
+    count: 0
+  };
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  render() {
+    return (
+      <div className="counter">
+        <h2>Counter</h2>
+        <div>
+          <button onClick={this.decrement}>-</button>
+          <span className="count">
+            {this.state.count}
+          </span>
+          <button onClick={this.increment}>+</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+// const Counter = () => (
+//   <div className="counter">
+//     <h2>Counter</h2>
+//     <div>
+//       <button>-</button>
+//       <span className="count">0</span>
+//       <button>+</button>
+//     </div>
+//   </div>
+// );
 
 ReactDOM.render(
   <Counter />,
